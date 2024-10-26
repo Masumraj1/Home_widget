b14b24554e0acf525265b02a2ff0019e8243e7f5
bdCalling <promotionbd321@gmail.com]>
2024-10-26 16:28:15 +0600

    some changes

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:home_widget/home_widget.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Use registerInteractivityCallback instead of registerBackgroundCallback
+  HomeWidget.registerInteractivityCallback(interactivityCallback);
+  runApp(const MyApp());
+}
+
+// Called when an interactivity event occurs from the widget
+Future<void> interactivityCallback(Uri? uri) async {
+  if (uri?.host == 'updatecounter') {
+    int counter = 0;
+    await HomeWidget.getWidgetData<int>('_counter', defaultValue: 0).then((value) {
+      counter = value!;
+      counter++;
+    });
+    await HomeWidget.saveWidgetData<int>('_counter', counter);
+    await HomeWidget.updateWidget(
+      name: 'HomeScreenWidgetProvider', // this must match the class name used in .kt file
+      iOSName: 'HomeScreenWidgetProvider',
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    HomeWidget.widgetClicked.listen((Uri? uri) => loadData());
+    loadData(); // This will load data from widget every time app is opened
+  }
+
+  void loadData() async {
+    await HomeWidget.getWidgetData<int>('_counter', defaultValue: 0).then((value) {
+      _counter = value!;
+    });
+    setState(() {});
+  }
+
+  Future<void> updateAppWidget() async {
+    await HomeWidget.saveWidgetData<int>('_counter', _counter);
+    await HomeWidget.updateWidget(
+      name: 'HomeScreenWidgetProvider',
+      iOSName: 'HomeScreenWidgetProvider',
+    );
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    updateAppWidget();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
